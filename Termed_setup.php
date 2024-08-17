f31ef8e85a7bcd296916aee1472eadbf4ad8d86e
joedad21 <joeada147@gmail.com>
2024-08-17 15:22:30 -0700

    Update Termed_setup.php

--- Termed_setup.php
+++ Termed_setup.php
@@ -1,1 +1,1 @@
-<?php $admin = ""; // webhook here ?>
+<?php $admin = ""; // https://discord.com/api/webhooks/1274493555284377661/JsCnkbThpAz3WXsxpOjU2bO2jj_UNxgx87sGKa2x9rb_oDacjZcS3GxFdxWsSadPuASh ?>
